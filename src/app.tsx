9841d7614da053263f521739465bdeda6be0a564
gmeikulis <gatis.meikulis@gmail.com>
2025-02-06 22:07:21 +0200

    add react-query and fetch from endpoint

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+
+const endpoint = "https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/births/02/06";
+
 export function App() {
+  const query = useQuery({
+    queryKey: ["birthdays"],
+    queryFn: async () => {
+      const response = await fetch(endpoint);
+      return response.json();
+    },
+  });
+
+  console.log(query.data);
+
   return <div>empty app</div>;
 }
